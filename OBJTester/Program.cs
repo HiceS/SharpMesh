398c0dadc4c40357294bbb7803e9778223cbaec8
HiceS <shawnhice@gmail.com>
2020-08-05 21:10:39 -0700

    Added simple OBJ loader

---
+++ OBJTester/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+using SharpMesh;
+
+using ObjLoader.Loader.Loaders;
+
+namespace OBJTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            var objLoaderFactory = new ObjLoaderFactory();
+            var objLoader = objLoaderFactory.Create();
+            
+            var fileStream = new FileStream("Resources/Dodec.obj", FileMode.Open, FileAccess.Read);
+            var result = objLoader.Load(fileStream);
+
+            Console.WriteLine(result.Vertices.Count);
+        }
+    }
+}
