f464ba872f9e75fc74d7d9f35a1003951a02e3d7
HiceS <shawnhice@gmail.com>
2020-08-13 11:38:58 -0700

    Created super simple way to find mesh bounds

---
+++ src/Decomposer/FindBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using SharpMesh.Data;
+
+namespace SharpMesh.Decomposer
+{
+    /// <summary>
+    /// A set of functions to find mesh bounds
+    /// </summary>
+    internal static class FindBounds
+    {
+        // To find the bounding
+        static Vector VectorBounds(IEnumerable<Vector> verts)
+        {
+            var temp = new Vector(0.0f, 0.0f, 0.0f);
+            
+            foreach (var vert in verts)
+            {
+                if (temp.X < vert.X)
+                    temp.X = vert.X;
+                
+                if (temp.Y < vert.Y)
+                    temp.Y = vert.Y;
+
+                if (temp.Z < vert.Z)
+                    temp.Z = vert.Z;
+            }
+            
+            return temp;
+        }
+    }
+}
