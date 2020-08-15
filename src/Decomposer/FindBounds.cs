fd6efe70d8ecae41735f9bf96d11c3a52cd9e304
HiceS <shawnhice@gmail.com>
2020-08-14 20:30:31 -0700

    Added true bounds

--- src/Decomposer/FindBounds.cs
+++ src/Decomposer/FindBounds.cs
@@ -3,29 +3,68 @@ using SharpMesh.Data;
 
 namespace SharpMesh.Decomposer
 {
+    public class Bounds
+    {
+        public float max_X;
+        public float min_X;
+        public float max_Y;
+        public float min_Y;
+        public float max_Z;
+        public float min_Z;
+
+        public Bounds()
+        {
+            
+        }
+
+    }
+    
+    
     /// <summary>
     /// A set of functions to find mesh bounds
     /// </summary>
     internal static class FindBounds
     {
         // To find the bounding
-        internal static Vector VectorBounds(IEnumerable<Vector<float>> verts)
+        internal static Bounds VectorBounds(IEnumerable<Vector<float>> verts)
         {
-            var temp = new Vector(0.0f, 0.0f, 0.0f);
+            var tempB = new Bounds();
             
             foreach (var vert in verts)
             {
-                if (temp.X < vert.X)
-                    temp.X = vert.X;
-                
-                if (temp.Y < vert.Y)
-                    temp.Y = vert.Y;
 
-                if (temp.Z < vert.Z)
-                    temp.Z = vert.Z;
+                if (vert.X < tempB.min_X)
+                {
+                    tempB.min_X = vert.X;
+                }
+                
+                if (vert.X > tempB.max_X)
+                {
+                    tempB.max_X = vert.X;
+                }
+                
+                if (vert.Y < tempB.min_Y)
+                {
+                    tempB.min_Y = vert.Y;
+                }
+                
+                if (vert.Y > tempB.max_Y)
+                {
+                    tempB.max_Y = vert.Y;
+                }
+                
+                if (vert.Z < tempB.min_Z)
+                {
+                    tempB.min_Z = vert.Z;
+                }
+                
+                if (vert.Z > tempB.max_Z)
+                {
+                    tempB.max_Z = vert.Z;
+                }
             }
             
-            return temp;
+            return tempB;
         }
     }
 }
