254a50d118b71d6f222efaaf6e01273cdc765268
Matt O'Tousa <matt@mattotousa.com>
2020-08-16 02:25:02 -0700

    removed non-working build for unity script

--- buildForUnityPlugin.sh
+++
@@ -1,6 +0,0 @@
-echo "Building..."
-dotnet build -o SharpMeshUnityPlugin/SharpMesh
-cd SharpMeshUnityPlugin/SharpMesh
-echo "Success! Cleaning up..."
-find . -type f,d ! -name "." ! -name 'SharpMesh.dll' -delete
-echo "Done!"
